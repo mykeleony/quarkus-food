53cd9e4eb7e0082ece149c1d81ad4ec2a844e7a6
Myke Amorim <mykeleony147@gmail.com>
2023-06-20 17:09:46 -0300

    refactor(restaurant/controller): implemented uses of mappers and services

--- register/src/main/java/com/github/mykeleony/api/controller/RestaurantController.java
+++ register/src/main/java/com/github/mykeleony/api/controller/RestaurantController.java
@@ -1,17 +1,44 @@
 package com.github.mykeleony.api.controller;
 
+import com.github.mykeleony.api.mapper.RestaurantMapper;
+import com.github.mykeleony.api.model.RestaurantInput;
 import com.github.mykeleony.domain.model.Restaurant;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
+import com.github.mykeleony.domain.service.RestaurantService;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
 
 @Path("/restaurants")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+@AllArgsConstructor
 public class RestaurantController {
     
+    private RestaurantMapper mapper;
+    private RestaurantService service;
+    
     @GET
-    public List<Restaurant> listAllRestaurants() {
-        return Restaurant.listAll();
+    public List<Restaurant> listAll() {
+        return service.listAll();
+    }
+    
+    @POST
+    public Restaurant create(RestaurantInput restaurantInput) {
+        Restaurant restaurant = mapper.toEntity(restaurantInput);
+        
+        return service.save(restaurant);
+    }
+    
+    @PUT
+    @Path("/{id}")
+    public Restaurant update(@PathParam("id") Long id, RestaurantInput restaurantInput) {
+        Restaurant restaurant = service.findById(id);
+        
+        return service.save(restaurant);
     }
 
 }
