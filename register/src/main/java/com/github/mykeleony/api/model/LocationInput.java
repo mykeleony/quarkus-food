94893b5fae97f05b83ccb2556a6e122fa1ee6875
Myke Amorim <mykeleony147@gmail.com>
2023-06-20 17:05:53 -0300

    feat(DTO): overridden toString method

--- register/src/main/java/com/github/mykeleony/api/model/LocationInput.java
+++ register/src/main/java/com/github/mykeleony/api/model/LocationInput.java
@@ -2,9 +2,11 @@ package com.github.mykeleony.api.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class LocationInput {
     
     private Double latitude;
--- register/src/main/java/com/github/mykeleony/api/model/RestaurantInput.java
+++ register/src/main/java/com/github/mykeleony/api/model/RestaurantInput.java
@@ -2,9 +2,11 @@ package com.github.mykeleony.api.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class RestaurantInput {
     
     private String name;
