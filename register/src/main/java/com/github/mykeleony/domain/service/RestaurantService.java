389359e6bf0f278350918c0b66937b15e303f15a
Myke Amorim <mykeleony147@gmail.com>
2023-06-20 17:07:16 -0300

    feat: created restaurant service

---
+++ register/src/main/java/com/github/mykeleony/domain/service/RestaurantService.java
@@ -0,0 +1,41 @@
+package com.github.mykeleony.domain.service;
+
+import com.github.mykeleony.domain.model.Restaurant;
+import com.github.mykeleony.domain.repository.RestaurantRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.NotFoundException;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+
+@ApplicationScoped
+@AllArgsConstructor
+@Slf4j
+public class RestaurantService {
+    
+    private RestaurantRepository restaurantRepository;
+    
+    public List<Restaurant> listAll() {
+        log.info("Listing all restaurants:");
+        
+        return restaurantRepository.listAll();
+    }
+    
+    public Restaurant findById(Long id) {
+        log.info("Searching restaurant with id {}:", id);
+        
+        return restaurantRepository.findByIdOptional(id).orElseThrow(NotFoundException::new);
+    }
+    
+    @Transactional
+    public Restaurant save(Restaurant restaurant) {
+        log.info("Saving restaurant {}:", restaurant);
+        
+        restaurantRepository.persist(restaurant);
+        
+        return restaurant;
+    }
+    
+}
