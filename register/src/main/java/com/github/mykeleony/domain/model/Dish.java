9c32952045c95524cc8e976e2dd26c95aac5cc42
Myke Amorim <mykeleony147@gmail.com>
2023-06-21 16:59:31 -0300

    feat(dishes): implemented compose key in Dish entity

--- register/src/main/java/com/github/mykeleony/domain/model/Dish.java
+++ register/src/main/java/com/github/mykeleony/domain/model/Dish.java
@@ -9,18 +9,19 @@ import java.math.BigDecimal;
 
 @Entity
 @Table(name = "dish")
+@IdClass(DishId.class)
 @Getter
 @Setter
 public class Dish extends PanacheEntityBase {
     
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
     private String name;
     private String description;
     private BigDecimal unitPrice;
     
+    @Id
     @ManyToOne
     @JoinColumn(name = "restaurant_id")
     private Restaurant restaurant;
