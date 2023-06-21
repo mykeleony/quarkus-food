8974b323ca4c7b44458ef7d3980a29f35b416c20
Myke Amorim <mykeleony147@gmail.com>
2023-06-21 12:07:34 -0300

    feat(dishes): implemented dish input->entity mapping

---
+++ register/src/main/java/com/github/mykeleony/api/mapper/DishMapper.java
@@ -0,0 +1,19 @@
+package com.github.mykeleony.api.mapper;
+
+import com.github.mykeleony.api.model.DishInput;
+import com.github.mykeleony.domain.model.Dish;
+import jakarta.enterprise.context.ApplicationScoped;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+
+@ApplicationScoped
+@AllArgsConstructor
+public class DishMapper {
+    
+    private ModelMapper mapper;
+    
+    public Dish toEntity(DishInput dishInput) {
+        return mapper.map(dishInput, Dish.class);
+    }
+    
+}
