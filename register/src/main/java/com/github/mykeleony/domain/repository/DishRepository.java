508c8bebd280b9dfb320ead33923fa445d13b1b1
Myke Amorim <mykeleony147@gmail.com>
2023-06-21 17:00:16 -0300

    feat(dishes): implemented dish counting by restaurant method

--- register/src/main/java/com/github/mykeleony/domain/repository/DishRepository.java
+++ register/src/main/java/com/github/mykeleony/domain/repository/DishRepository.java
@@ -1,9 +1,15 @@
 package com.github.mykeleony.domain.repository;
 
 import com.github.mykeleony.domain.model.Dish;
+import com.github.mykeleony.domain.model.Restaurant;
 import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
 import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 public class DishRepository implements PanacheRepositoryBase<Dish, Long> {
+
+    public long countByRestaurant(Restaurant restaurant) {
+        return count("restaurant", restaurant);
+    }
+
 }
