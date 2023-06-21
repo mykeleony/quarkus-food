bf354f6f27d4b163d65ffc1f1f69f1b348cf47fa
Myke Amorim <mykeleony147@gmail.com>
2023-06-21 17:10:54 -0300

    fix(dishes): changed repository's id parameter to composed one

--- register/src/main/java/com/github/mykeleony/domain/repository/DishRepository.java
+++ register/src/main/java/com/github/mykeleony/domain/repository/DishRepository.java
@@ -1,12 +1,13 @@
 package com.github.mykeleony.domain.repository;
 
 import com.github.mykeleony.domain.model.Dish;
+import com.github.mykeleony.domain.model.DishId;
 import com.github.mykeleony.domain.model.Restaurant;
 import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
 import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
-public class DishRepository implements PanacheRepositoryBase<Dish, Long> {
+public class DishRepository implements PanacheRepositoryBase<Dish, DishId> {
 
     public long countByRestaurant(Restaurant restaurant) {
         return count("restaurant", restaurant);
