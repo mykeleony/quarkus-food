5cae7a732ee893ca6f301287313e1e9666fe4848
Myke Amorim <mykeleony147@gmail.com>
2023-06-23 16:48:02 -0300

    feat(test): created constant with list of restaurants

--- register/src/test/java/com/github/mykeleony/common/RestaurantConstants.java
+++ register/src/test/java/com/github/mykeleony/common/RestaurantConstants.java
@@ -5,6 +5,8 @@ import com.github.mykeleony.domain.model.Restaurant;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
 
 public class RestaurantConstants {
     
@@ -12,5 +14,15 @@ public class RestaurantConstants {
     private static final LocalDate DATE = LocalDate.of(2023, Month.JUNE, 22);
     
     public static final Restaurant RESTAURANT = new Restaurant("restaurant", "owner", "55.351.632/0001-91", LOCATION, DATE, DATE);
+    
+    public static final List<Restaurant> RESTAURANTS = new ArrayList<>() {
+        {
+            int restaurantsAmount = 3;
+            
+            for (int i = 0; i < restaurantsAmount; i++) {
+                add(RESTAURANT);
+            }
+        }
+    };
     
 }
