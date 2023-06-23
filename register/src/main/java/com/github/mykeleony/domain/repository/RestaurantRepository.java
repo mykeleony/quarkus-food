d387c7f206d01b316e3dcbdbc54a9f0fa92a6672
Myke Amorim <mykeleony147@gmail.com>
2023-06-23 20:22:03 -0300

    fix(restaurants): repository now implements PanacheRepositoryBase interface

--- register/src/main/java/com/github/mykeleony/domain/repository/RestaurantRepository.java
+++ register/src/main/java/com/github/mykeleony/domain/repository/RestaurantRepository.java
@@ -1,9 +1,9 @@
 package com.github.mykeleony.domain.repository;
 
 import com.github.mykeleony.domain.model.Restaurant;
-import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
 import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
-public class RestaurantRepository implements PanacheRepository<Restaurant> {
+public class RestaurantRepository implements PanacheRepositoryBase<Restaurant, Long> {
 }
