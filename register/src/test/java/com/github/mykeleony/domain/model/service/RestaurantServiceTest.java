179c3dba00619d815d92be92cb1b86335496b679
Myke Amorim <mykeleony147@gmail.com>
2023-06-23 16:59:34 -0300

    feat(test): implemented search tests by id

--- register/src/test/java/com/github/mykeleony/domain/model/service/RestaurantServiceTest.java
+++ register/src/test/java/com/github/mykeleony/domain/model/service/RestaurantServiceTest.java
@@ -1,21 +1,22 @@
 package com.github.mykeleony.domain.model.service;
 
-import com.github.mykeleony.common.RestaurantConstants;
 import com.github.mykeleony.domain.model.Restaurant;
 import com.github.mykeleony.domain.repository.RestaurantRepository;
 import com.github.mykeleony.domain.service.RestaurantService;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.junit.mockito.InjectMock;
 import jakarta.inject.Inject;
-import org.assertj.core.api.Assertions;
+import jakarta.ws.rs.NotFoundException;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
 import java.util.List;
+import java.util.Optional;
 
 import static com.github.mykeleony.common.RestaurantConstants.RESTAURANT;
 import static com.github.mykeleony.common.RestaurantConstants.RESTAURANTS;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 
 @QuarkusTest
@@ -42,6 +43,26 @@ public class RestaurantServiceTest {
     @Test
     public void listRestaurants_withoutRegisteredRestaurants_ReturnsEmpty() {
         assertThat(service.listAll()).isEmpty();
+    }
+    
+    @Test
+    public void findRestaurant_WithExistingId_ReturnsRestaurant() {
+        when(repository.findByIdOptional(anyLong())).thenReturn(Optional.of(RESTAURANT));
+        
+        Restaurant sut = service.findById(1L);
+        
+        assertThat(sut).isNotNull();
+        assertThat(sut.getName()).isEqualTo(RESTAURANT.getName());
+        assertThat(sut.getCnpj()).isEqualTo(RESTAURANT.getCnpj());
+        assertThat(sut.getOwner()).isEqualTo(RESTAURANT.getOwner());
+        assertThat(sut.getLocation()).isEqualTo(RESTAURANT.getLocation());
+        assertThat(sut.getCreationDate()).isEqualTo(RESTAURANT.getCreationDate());
+        assertThat(sut.getUpdateDate()).isEqualTo(RESTAURANT.getUpdateDate());
+    }
+    
+    @Test
+    public void findRestaurant_WithNonexistentId_ThrowsException() {
+        assertThatThrownBy(() -> service.findById(99L)).isInstanceOf(NotFoundException.class);
     }
     
 }
